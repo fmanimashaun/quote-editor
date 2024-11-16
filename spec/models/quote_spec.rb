dcbf92546858e0c8493881a591898a41bfc58f42
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-11-16 08:32:24 +0100

    Update tests

--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Quote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "With a valid factory" do
+    let!(:quote) { build(:quote) }
+
+    it "has a valid factory" do
+      expect(quote).to be_a(Quote)
+      expect(quote).to be_valid
+    end
+  end
+
+  context "Validations" do
+    let!(:quote) { create(:quote) }
+
+    it "has a valid name attribute" do
+      expect(quote.name).to be_present
+      expect(quote.name).to be_a(String)
+    end
+
+    it "is invalid without a name" do
+      quote.name = nil
+      expect(quote).not_to be_valid
+      expect(quote.errors[:name]).to include("can't be blank")
+    end
+  end
 end
